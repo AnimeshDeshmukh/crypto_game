9a8097155eac4f90946119ec633478fff27ac7a7
AnimeshDeshmukh <50483840+AnimeshDeshmukh@users.noreply.github.com>
2019-05-10 15:45:37 +0530

    Add files via upload

---
+++ touch_controls/Assets/coins.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coins : MonoBehaviour {
+    public float delay = 5f;
+    public GameObject coin;
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("Spawn", delay, 1f*delay);
+	}
+	
+	// Update is called once per frame
+	void Spawn () {
+        Instantiate(coin, new Vector2(Random.Range(-2, 2), 10), Quaternion.identity);
+	}
+}
